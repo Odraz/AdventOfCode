be6f6d0bdb63ce8db1c4024a16eaaf86fa4ec54b
Tomáš Pouzar <Tomas.Pouzar@csw.cz>
2023-12-09 07:42:20 +0100

    Minor refactoring

--- AdventOfCode23/Day09/Day09.cs
+++ AdventOfCode23/Day09/Day09.cs
@@ -1,4 +1,3 @@
-using System.Text.RegularExpressions;
 using AdventOfCode.Interfaces;
 
 public class Day09 : IDay
@@ -12,37 +11,45 @@ public class Day09 : IDay
 
     public object SolveOne()
     {
-        IEnumerable<int?[]> histories = lines.Select(l => l.Split(' ').Select(match => (int?)int.Parse(match)).ToArray());
+        IEnumerable<int?[]> histories = GetHistories();
 
         int lastIndex = histories.First().Length - 1;
 
         return histories.Select(h => PredictNext(lastIndex, h)).Sum();
     }
 
+    private IEnumerable<int?[]> GetHistories() =>
+        lines.Select(l => l.Split(' ').Select(match => (int?)int.Parse(match)).ToArray());
+
     private long PredictNext(int lastIndex, int?[] history)
     {
         if (history.All(r => r == 0 || r == null))
             return 0;
         else
-            return PredictNext(lastIndex, history.Select((r, index) => index == lastIndex ? null : (history[index + 1] == null ? null : history[index + 1] - r)).ToArray()) +
+            return PredictNext(lastIndex, history.Select((r, index) => GetDiff(r, index, lastIndex, history)).ToArray()) +
                    history.Last(r => r != null)!.Value;
     }
 
     public object SolveTwo()
     {
-        IEnumerable<int?[]> histories = lines.Select(l => l.Split(' ').Select(match => (int?)int.Parse(match)).ToArray());
+        IEnumerable<int?[]> histories = GetHistories();
 
         int lastIndex = histories.First().Length - 1;
 
         return histories.Select(h => PredictPrevious(lastIndex, h)).Sum();
     }
-    
+
     private long PredictPrevious(int lastIndex, int?[] history)
     {
         if (history.All(r => r == 0 || r == null))
             return 0;
         else
-            return  history.First()!.Value -
-                    PredictPrevious(lastIndex, history.Select((r, index) => index == lastIndex ? null : (history[index + 1] == null ? null : history[index + 1] - r)).ToArray());
+            return history.First()!.Value -
+                   PredictPrevious(lastIndex, history.Select((r, index) => GetDiff(r, index, lastIndex, history)).ToArray());
     }
+
+    private static int? GetDiff(int? r, int index, int lastIndex, int?[] history) =>
+        index == lastIndex || (history[index + 1] == null) ?
+            null :
+            history[index + 1] - r;
 }
